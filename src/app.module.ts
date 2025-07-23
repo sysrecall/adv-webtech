6cca0bbe925e2ccaf1fa99e3908c19382a34231b
sysrecall <uprojectemail@protonmail.com>
2025-07-23 20:28:25 +0600

    Update: Remove and Add appropriate user types

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { ArtistModule } from './artist/artist.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, ArtistModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
