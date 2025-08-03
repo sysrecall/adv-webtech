cb2e26ea037d94f139229ef60c5ee4d3a6c9a2cb
Sabed Bin Sarwar <sabedbinsarwar@gmail.com>
2025-08-03 23:07:58 +0600

    create-artist.dto.ts

--- src/artist/dto/create-artist.dto.ts
+++ src/artist/dto/create-artist.dto.ts
@@ -1,30 +1,8 @@
-import { IsString, Matches, IsDateString, IsUrl, IsOptional } from 'class-validator';
+import { IsOptional, IsString, Length } from 'class-validator';
 
 export class CreateArtistDto {
+  @IsOptional()
   @IsString()
-  @Matches(/^[A-Za-z\s]+$/, {
-    message: 'Name must not contain numbers or special characters',
-  })
-  name: string;
-
-  @IsString()
-  @Matches(/^(?=.*[@#$&]).+$/, {
-    message: 'Password must contain at least one special character (@, #, $, &)',
-  })
-  password: string;
-
-  @IsDateString({}, { message: 'Date must be a valid ISO date string' })
-  birthDate: string;
-
-  @IsOptional()
-  @IsUrl({}, { message: 'Facebook link must be a valid URL' })
-  facebookLink?: string;
-
-  @IsOptional()
-  @IsUrl({}, { message: 'Twitter link must be a valid URL' })
-  twitterLink?: string;
-
-  @IsOptional()
-  @IsUrl({}, { message: 'Instagram link must be a valid URL' })
-  instagramLink?: string;
+  @Length(1, 30)
+  country?: string;
 }
