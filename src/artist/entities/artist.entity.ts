c37317a181495b9b9dd87ccdd34cc57284ed5724
Sabed Bin Sarwar <sabedbinsarwar@gmail.com>
2025-08-03 23:12:27 +0600

    artist.entity.ts

--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,9 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('artists')
 export class Artist {
+  @PrimaryGeneratedColumn()
   id: number;
-  name: string;
-  password: string;
-  birthDate: string;
-  facebookLink?: string;
-  twitterLink?: string;
-  instagramLink?: string;
+
+  @Column({ type: 'varchar', length: 150, unique: true })
+  uniqueId: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  joiningDate: Date;
+
+  @Column({ type: 'varchar', length: 30, default: 'Unknown' })
+  country: string;
+
+  @BeforeInsert()
+  generateUuid() {
+    this.uniqueId = uuidv4();
+  }
 }
