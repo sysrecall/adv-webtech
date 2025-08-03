517516545833b6dd5ccb2b20d2d3c16fb29a5cda
Sabed Bin Sarwar <sabedbinsarwar@gmail.com>
2025-08-03 23:11:21 +0600

    artist.controller.ts

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,41 +1,37 @@
-import {Controller,Get,Post,Patch,Delete,Param,Body,Query,ParseIntPipe,} from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Get,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
-import { UpdateArtistDto } from './dto/update-artist.dto';
+import { UpdateArtistCountryDto } from './dto/update-artist-country.dto';
 
 @Controller('artist')
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistService.create(createArtistDto);
-  }
-
-  @Get()
-  find(@Query('name') name?: string) {
-    if (name) {
-      return this.artistService.findByName(name);
-    }
-    return this.artistService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.artistService.findOne(id);
+  create(@Body() dto: CreateArtistDto) {
+    return this.artistService.create(dto);
   }
 
   @Patch(':id')
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateArtistDto: UpdateArtistDto,
-  ) {
-    return this.artistService.update(id, updateArtistDto);
+  updateCountry(@Param('id') id: string, @Body() dto: UpdateArtistCountryDto) {
+    return this.artistService.updateCountry(+id, dto);
   }
 
-  @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    this.artistService.remove(id);
-    return { message: `Artist with ID ${id} deleted successfully.` };
+  @Get('by-date')
+  findByJoiningDate(@Query('date') date: string) {
+    return this.artistService.findByJoiningDate(date);
+  }
+
+  @Get('unknown-country')
+  findByUnknownCountry() {
+    return this.artistService.findByUnknownCountry();
   }
 }
