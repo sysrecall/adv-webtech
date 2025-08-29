4bfa1f8db8f8151e74ad328a5498661d682d12cc
sysrecall <uprojectemail@protonmail.com>
2025-08-29 22:25:27 +0600

    Add: Populate Customer Login, Register Components

--- frontend/app/customer/login/page.tsx
+++ frontend/app/customer/login/page.tsx
@@ -1,26 +1,9 @@
+import LoginForm from "../components/LoginForm";
+
 export default function Login() {
     return (
         <>
-            <form method="post">
-                <table>
-                <tbody>
-                    <tr>
-                        <td>Username</td>
-                        <td><input name="username" /> </td>
-                    </tr>
-                    <tr>
-                       <td>Password</td>
-                       <td><input name="password" type="password" /></td> 
-                    </tr>
-                    <tr>
-                        <td>
-                            <button type="submit">Login</button>
-                        </td>
-                    </tr>
-
-                </tbody>
-                </table>
-            </form>
+            <LoginForm /> 
         </>
     );
 }
--- frontend/app/customer/register/page.tsx
+++ frontend/app/customer/register/page.tsx
@@ -1,38 +1,9 @@
+import RegisterForm from "../components/RegisterForm";
+
 export default function Register() {
     return (
         <>
-            <form method="post">
-                <table>
-                <tbody>
-                    <tr>
-                        <td>Email</td>
-                        <td><input name="email" type="email"/> </td>
-                    </tr>
-                    <tr>
-                       <td>Full Name</td>
-                       <td><input name="fullName" /></td> 
-                    </tr>
-                    <tr>
-                        <td>Username</td>
-                        <td><input name="username" /> </td>
-                    </tr>
-                    <tr>
-                       <td>Password</td>
-                       <td><input name="password" type="password" /></td> 
-                    </tr>
-                    <tr>
-                       <td>Confirm Password</td>
-                       <td><input type="password" /></td> 
-                    </tr>
-                    <tr>
-                        <td>
-                            <button type="submit">Register</button>
-                        </td>
-                    </tr>
-
-                </tbody>
-                </table>
-            </form>
+            <RegisterForm />
         </>
     );
 }
